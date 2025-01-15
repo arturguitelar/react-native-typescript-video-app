d95f42a3980c53497d72c608de9f366c0d978ee5
Artur Guitelar <artur.guitelar@gmail.com>
2025-01-15 20:43:47 -0300

    Fix tailwind configs

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,13 @@
 import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>:/</Text>
+    <View className="flex flex-1 items-center justify-center">
+      <Text className="text-3xl">:/</Text>
       <Link href={{ pathname: '/profile' }} style={{ color: 'blue' }}>
         Go to Profile
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function Profile() {
   return (
     <View>
-      <Text>Profile</Text>
+      <Text>Profile :D</Text>
     </View>
   );
 }
