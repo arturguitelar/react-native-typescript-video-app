911aef7e2b651a8a984c3860429adec02fe5865d
Artur Guitelar <artur.guitelar@gmail.com>
2025-01-15 20:34:16 -0300

    Add & Config NativeWind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
+import '../styles/global.css';
+
 export default function RootLayout() {
   return (
     <>
