522a47e7442b6dde0ffe5aced59f4f538fc4af76
Artur Guitelar <artur.guitelar@gmail.com>
2025-01-15 20:27:30 -0300

    Add initial routes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,15 @@
-import { Stack } from "expo-router";
+import React from 'react';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="profile" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>:/</Text>
+      <Link href={{ pathname: '/profile' }} style={{ color: 'blue' }}>
+        Go to Profile
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
