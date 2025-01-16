fd4a86acd432540ba77b22c04395575cb4890503
Artur Guitelar <artur.guitelar@gmail.com>
2025-01-15 21:31:23 -0300

    Config fonts and layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function Index() {
   return (
     <View className="flex flex-1 items-center justify-center">
-      <Text className="text-3xl">:/</Text>
+      <Text className="text-3xl font-pblack">:/</Text>
       <Link href={{ pathname: '/profile' }} style={{ color: 'blue' }}>
         Go to Profile
       </Link>
