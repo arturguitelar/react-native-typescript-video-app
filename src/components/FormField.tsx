13b579508a587cf086f0b50562d2ad50fd010f03
Artur Guitelar <artur.guitelar@gmail.com>
2025-01-24 17:39:32 -0300

    Update Upload Video page

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -25,7 +25,7 @@ export function FormField({
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
 
-      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
+      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row mt-2">
         <TextInput
           className="flex-1 text-white font-psemibold text-base"
           value={value}
